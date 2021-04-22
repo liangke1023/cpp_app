d8be8bb47ff0be6b8100d189d901adfe290d35a4
Lei Zheng <zhenglei0418@gmail.com>
2021-04-22 22:40:27 +0200

    update

--- main.cpp
+++ main.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main()
 {
-
-    int a = 0;
-
-    int b = 10;
-
-    int c = 100;
-
-    cout<<"hello world!"<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
 }
