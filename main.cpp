9bcb3c636bef08325b9e337a60d73f8152cfabde
Lei Zheng <zhenglei0418@gmail.com>
2021-10-21 21:34:46 +0200

    1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+class Graph {
+ private:
+  bool **graph;
+  float density;
+  int size;
+
+ public:
+  Graph(int size = 50, float density = 0.3);
+};
+Graph::Graph(int size, float density) : size(size), density(density) {
+  cout << "size " << size << " density " << density << endl;
+}
+
 int main(int argc, char **argv) {
-  int a = 0;
-  cout << "hello world" << endl;
+  Graph g;
   return 0;
 }
