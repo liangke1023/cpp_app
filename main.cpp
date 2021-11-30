ab016dfa8a74b42d122b60b9a064e3d93b5e0e2e
Lei Zheng <zhenglei0418@gmail.com>
2021-11-30 22:06:30 +0100

    test ifstream

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
+#include <fstream>
 #include <iostream>
 #include <list>
+#include <vector>
 
-#include "shortest_path.h"
 using namespace std;
 
 int main(int argc, char** argv) {
-  float densities[2] = {0.2f, 0.4f};
-  const int size = 50;
-  for (auto d : densities) {
-    ShortestPath shortest_path(size, d, 1, 10);
-    shortest_path.vertices();
-    int sum_shortest = 0;
-    int paths_exist = 0;
-    for (int i = 1; i < size; i++) {
-      int path_size = shortest_path.path_size(0, i);
-      if (path_size > 0) {
-        sum_shortest += path_size;
-        paths_exist++;
-      }
-    }
-    cout << "average of shortest paths: "
-         << static_cast<double>(sum_shortest) / paths_exist << endl;
-  }
-
+  ifstream file("SampleTestData");
+  istream_iterator<int> begin(file), end;
+  vector<int> graph(begin, end);
+  cout << graph.size() << endl;
+  for (int i : graph) cout << i << " ";
   return 0;
 }
